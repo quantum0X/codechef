088dd9478197897b05296cc7718e240fbe801309
quantum0X <thevishal2108@gmail.com>
2022-03-08 03:03:32 +0530

    JASSIGNMENT

---
+++ JASSIGNMENT-Janmansh_and_Assingment.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+	while(T--){
+	    int i;
+	    cin >> i;
+	    i+3 <= 10 ? cout << "Yes" << endl : cout << "No" << endl;
+	}
+	return 0;
+} 
