d81bfdfb58331b21042bb5e984e317134946fac1
quantum0X <thevishal2108@gmail.com>
2022-08-13 00:31:11 +0530

    MAXIMUMSUBS

---
+++ August Long 2022 Division 4/MAXIMUMSUBS.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastIO ios_base::sync_with_stdio(false), cin.tie(NULL);
+
+int main() {
+	// your code goes here
+	fastIO;
+	int T;
+	cin >> T;
+	while(T--) {
+	    int X;
+	    cin >> X;
+	    cout << X*2 << "\n";
+	}
+	return 0;
+}
